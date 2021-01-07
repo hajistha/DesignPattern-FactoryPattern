2a7c55567618c69e707e091318da8227aaabd494
Hajistha <51606129+hajistha@users.noreply.github.com>
2021-01-07 08:43:09 -0800

    Add files via upload

---
+++ FactoryPattern/FactoryPattern/com.factory/Divide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.com.factory
+{
+   public class Divide : ICalculate
+    {
+        public void Calculate(double a, double b)
+        {
+            Console.WriteLine("a/b is {0}", a / b);
+        }
+    }
+}
---
+++ FactoryPattern/FactoryPattern/com.factory/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.com.factory
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number");
+            string input = Console.ReadLine();
+            double num1, num2;
+            bool result = Double.TryParse(input, out num1);
+            if (!result)
+            {
+                Console.WriteLine("please enter a number");
+                return;
+            }
+            Console.WriteLine("Enter Second number");
+            result = Double.TryParse(Console.ReadLine(), out num2);
+            if (!result)
+            {
+                Console.WriteLine("please enter a number");
+                return;
+            }
+
+            Console.WriteLine("Enter Add, Subtract or Divide");
+            CalculateFactory factory = new CalculateFactory();
+            ICalculate obj = factory.GetCalculation(Console.ReadLine());
+
+            //Divide obj = new Divide();
+            obj.Calculate(num1, num2);
+            Main();
+
+        }
+    }
+}
